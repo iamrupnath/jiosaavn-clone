b2d40a689ba644b08b3b67c0adea771bd8ff7b46
Rupnath Shaw <93729247+iamrupnath@users.noreply.github.com>
2024-02-12 19:13:19 +0530

    Update AlbumDetails.jsx

--- src/pages/AlbumDetails.jsx
+++ src/pages/AlbumDetails.jsx
@@ -15,7 +15,7 @@ const AlbumDetails = () => {
   const { id } = useParams();
 
   const getAlbumDetails = async () => {
-    const res = await axios.get(`https://saavn.me/albums?id=${id}`);
+    const res = await axios.get(`https://saavn.dev/albums?id=${id}`);
     const { data } = await res.data;
     console.log(data);
     setAlbum(data);
